533528e443eab7a9b9a19d8d4372ea6c5b5b0b63
Andrés F Rave <arave@contentobps.com>
2019-01-17 16:05:44 -0500

    Prueba 1245 telefonia

--- procesos/Telefonia/login_logout.py
+++ procesos/Telefonia/login_logout.py
@@ -87,7 +87,7 @@ def Ejecutar():
                 data = (data +str(rown["date"])+"|"+str(rown["agent"])+"|"+str(rown["identification"])+"|"+str(rown["login_date"])+"|"+str(rown["logout_date"])+"|"+str(rown["login_time"])+"|"+ str(row.ipdial_code) + "|" + str(row.id_cliente) + "|" + str(row.cartera)) + "/n"
     file.close()
     ejecutar = login_logout_beam.run(data)
-    os.remove("/media/BI_Archivos/GOOGLE/Telefonia/Login_out.txt")
+    # os.remove("/media/BI_Archivos/GOOGLE/Telefonia/Login_out.txt")
     return ("Proceso de listamiento de datos: listo ..........................................................." + ejecutar)
 
 ########################################################################################################################

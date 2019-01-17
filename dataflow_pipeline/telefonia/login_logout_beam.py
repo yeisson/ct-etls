0d93c1e4bc03894b0405d0393616b2bcdfc1cfe0
Andrés F Rave <arave@contentobps.com>
2019-01-17 16:15:42 -0500

    Prueba 1346 telefonia

--- dataflow_pipeline/telefonia/login_logout_beam.py
+++ dataflow_pipeline/telefonia/login_logout_beam.py
@@ -122,7 +122,8 @@ def run(data):
     ])
 
 	lines = pipeline | 'Lectura de Archivo' >> ReadFromText("/media/BI_Archivos/GOOGLE/Telefonia/Login_out.txt")
-	lines | 'Escribir en Archivo' >> WriteToText(gcs_path + "/Login_out/" + fecha, file_name_suffix='.txt',shard_name_template='')
+	lines | 'Escribir en Archivo' >> WriteToText("/media/BI_Archivos/GOOGLE/Telefonia/Login_out", file_name_suffix='.txt',shard_name_template='')
+	# lines | 'Escribir en Archivo' >> WriteToText(gcs_path + "/Login_out/" + fecha, file_name_suffix='.txt',shard_name_template='')
 	# transformed = (lines | 'Formatear Data' >> beam.ParDo(formatearData()))
 	# transformed | 'Escritura a BigQuery Telefonia' >> beam.io.WriteToBigQuery(
 	# 	gcs_project + ":telefonia.login_logout", 

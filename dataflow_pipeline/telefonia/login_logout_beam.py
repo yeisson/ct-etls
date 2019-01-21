a8674a928cf5bb0d655460bbc8ee243d88c461b6
Andrés F Rave <arave@contentobps.com>
2019-01-21 15:42:14 -0500

    prueba 461 telefonia

--- dataflow_pipeline/telefonia/login_logout_beam.py
+++ dataflow_pipeline/telefonia/login_logout_beam.py
@@ -34,6 +34,7 @@ import socket
 import argparse
 import uuid
 import datetime
+from flask import Flask, request, jsonify, redirect
 import apache_beam as beam
 from apache_beam.io import ReadFromText
 from apache_beam.io import WriteToText
@@ -122,6 +123,9 @@ def run():
     ])
 
 	lines = pipeline | 'Lectura de Archivo' >> ReadFromText("/media/BI_Archivos/GOOGLE/Telefonia/Login_out.csv")
+	
+	
+
 	# lines | 'Escribir en Archivo' >> beam.io.WriteToText(gcs_path + "/Login_out/" + fecha, file_name_suffix='.txt',shard_name_template='')
 	# lines | 'Escribir en Archivo' >> WriteToText("/media/BI_Archivos/GOOGLE/Telefonia/x/Login_out2", file_name_suffix='.csv',shard_name_template='')
  	# lines | 'Escribir en Archivo' >> WriteToText("archivos/Base_Marcada_small", file_name_suffix='.csv',shard_name_template='')
@@ -134,7 +138,7 @@ def run():
 	# 	)
 	jobObject = pipeline.run()
 	return ("Proceso de transformacion y cargue, completado")
-	# return (transformed)
+	return jsonify(lines)
 
 
 ################################################################################

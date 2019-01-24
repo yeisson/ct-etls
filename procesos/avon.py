436b772e74f5eb789429939e075b02d1850d7a50
lcanor <andreacanorestrepo74@gmail.com>
2019-01-24 12:39:17 -0500

    actualzacion de infore balance

--- procesos/avon.py
+++ procesos/avon.py
@@ -68,3 +68,47 @@ def archivos_Seguimiento():
 
     return jsonify(response), response["code"]
     # return "Corriendo : " + mensaje    
+
+    @avon_api.route("/archivos_Balance")
+def archivos_Seguimiento():
+
+    response = {}
+    response["code"] = 400
+    response["description"] = "No se encontraron ficheros"
+    response["status"] = False
+
+    local_route = fileserver_baseroute + "/BI_Archivos/GOOGLE/Avon/Balance/"
+    archivos = os.listdir(local_route)
+    for archivo in archivos:
+        if archivo.endswith(".txt"):
+            mifecha = archivo[8:16]
+
+            storage_client = storage.Client()
+            bucket = storage_client.get_bucket('ct-avon')
+
+            # Subir fichero a Cloud Storage antes de enviarlo a procesar a Dataflow
+            blob = bucket.blob('Balance/' + archivo)
+            blob.upload_from_filename(local_route + archivo)
+
+            # Una vez subido el fichero a Cloud Storage procedemos a eliminar los registros de BigQuery
+            deleteQuery = "DELETE FROM `contento-bi.avon_Balance` WHERE fecha = '" + mifecha + "'"
+
+            #Primero eliminamos todos los registros que contengan esa fecha
+            client = bigquery.Client()
+            query_job = client.query(deleteQuery)
+
+            #result = query_job.result()
+            query_job.result() # Corremos el job de eliminacion de datos de BigQuery
+
+            # Terminada la eliminacion de BigQuery y la subida a Cloud Storage corremos el Job
+            mensaje = avon_pagos_beam.run('gs://ct-avon/Balance/' + archivo, mifecha)
+            if mensaje == "Corrio Full HD":
+                move(local_route + archivo, fileserver_baseroute + "/BI_Archivos/GOOGLE/Avon/Balance/Procesados/"+archivo)
+                response["code"] = 200
+                response["description"] = "Se realizo la peticion Full HD"
+                response["status"] = True
+
+    return jsonify(response), response["code"]
+    # return "Corriendo : " + mensaje    
+
+    

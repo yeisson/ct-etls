10acff3049c4d4f9f8f00a896ef4ae4e4c917e7a
Andrés F Rave <arave@contentobps.com>
2019-02-08 11:54:38 -0500

    Creación del removedor de archivos del storage

---
+++ procesos/Telefonia/remover.py
@@ -0,0 +1,87 @@
+####################################################################################################
+##                                 ELIMINAR ARCHIVOS DEL STORAGE                                  ##
+####################################################################################################
+
+
+########################### LIBRERIAS #####################################
+from flask import Blueprint
+from flask import jsonify
+from google.oauth2 import service_account
+from google.auth.transport.requests import AuthorizedSession
+from google.cloud import datastore
+from google.cloud import bigquery
+from google.cloud import storage
+import logging
+import uuid
+import json
+import urllib3
+import socket
+import requests
+import os
+import dataflow_pipeline.massive as pipeline
+import cloud_storage_controller.cloud_storage_controller as gcscontroller
+import datetime
+import time
+import dataflow_pipeline.telefonia.remover_beam as remover_beam
+
+remover_api = Blueprint('remover_api', __name__)
+#############################################################################
+
+
+
+########################### DEFINICION DE VARIABLES ###########################
+
+ayer = datetime.datetime.today() - datetime.timedelta(days = 2)
+ano = str(ayer.year)
+if len(str(ayer.day)) == 1:
+    dia = "0" + str(ayer.day)
+else:
+    dia = str(ayer.day)
+if len(str(ayer.month)) == 1:
+    mes = "0"+ str(ayer.month)
+else:
+    mes = str(ayer.month)
+
+
+fecha = str(ano)+str(mes)+str(dia)
+Ruta ="media"
+Ruta_Alterna ="/192.168.20.87"
+KEY_REPORT = "remover"
+fileserver_baseroute = ("//192.168.20.87", "/media")[socket.gethostname()=="contentobi"]
+
+########################### CODIGO #####################################################################################
+
+@remover_api.route("/" + KEY_REPORT)
+def Ejecutar():
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket('ct-telefonia')
+    gcs_path = 'gs://ct-telefonia'
+    ext = ".csv"
+
+    blob1 = bucket.blob("agent_status/" + fecha + ext)
+    blob2 = bucket.blob("cdr/" + fecha + ext)
+    blob3 = bucket.blob("csat/" + fecha + ext)
+    blob4 = bucket.blob("login_logout/" + fecha + ext)
+
+    
+    try:
+        blob1.delete()
+    except: 
+        print("No hay archivo que borrar")
+    
+    try:
+        blob2.delete()
+    except: 
+        print("No hay archivo que borrar")
+    
+    try:
+        blob3.delete()
+    except: 
+        print("No hay archivo que borrar")
+    
+    try:
+        blob4.delete()
+    except: 
+        print("No hay archivo que borrar")
+    
+    return("Los archivos fueron eliminados con exito")

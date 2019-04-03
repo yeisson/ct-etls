4361bdfd977f55ca2601a3b34e67dc06ffe839f1
Andrés F Rave <arave@contentobps.com>
2019-04-03 16:55:17 -0500

    clear

--- procesos/PhpTOPython/mirror.py
+++ procesos/PhpTOPython/mirror.py
@@ -20,7 +20,8 @@ fileserver_baseroute = ("//192.168.20.87", "/media")[socket.gethostname()=="cont
 
 
 #####################################################################################################################################
-##################################################### BD_CONSOLIDADO ################################################################
+#####################################################################################################################################
+######################################################## DELETE #####################################################################
 #####################################################################################################################################
 #####################################################################################################################################
 
@@ -42,3 +43,60 @@ def delete():
 
     print("Proceso de eliminacion Completado")
     return "La siguiente informacion proviente de python: " + id_cliente + "," + producto + "," + sub_producto
+
+
+
+
+
+#####################################################################################################################################
+#####################################################################################################################################
+######################################################## LOAD #######################################################################
+#####################################################################################################################################
+#####################################################################################################################################
+
+
+
+# @mirror_api.route("/load", methods=['GET'])
+# def load():
+
+# #Parametros GET para modificar la consulta segun los parametros entregados
+#     ruta = request.args.get('mi_archivo')
+
+
+#     response = {}
+#     response["code"] = 400
+#     response["description"] = "No se encontraron ficheros"
+#     response["status"] = False
+
+#     local_route = fileserver_baseroute + "/" + ruta
+#     archivos = os.listdir(local_route)
+#     for archivo in archivos:
+#         if archivo.endswith(".csv"):
+#             mifecha = archivo[20:28]
+
+#             storage_client = storage.Client()
+#             bucket = storage_client.get_bucket('ct-bancolombia')
+
+#             # Subir fichero a Cloud Storage antes de enviarlo a procesar a Dataflow
+#             blob = bucket.blob('info-seguimiento/' + archivo)
+#             blob.upload_from_filename(local_route + archivo)
+
+#             # Una vez subido el fichero a Cloud Storage procedemos a eliminar los registros de BigQuery
+#             deleteQuery = "DELETE FROM `contento-bi.bancolombia_admin.seguimiento` WHERE fecha = '" + mifecha + "'"
+
+#             #Primero eliminamos todos los registros que contengan esa fecha
+#             client = bigquery.Client()
+#             query_job = client.query(deleteQuery)
+
+#             #result = query_job.result()
+#             query_job.result() # Corremos el job de eliminacion de datos de BigQuery
+
+#             # Terminada la eliminacion de BigQuery y la subida a Cloud Storage corremos el Job
+#             mensaje = bancolombia_seguimiento_beam.run('gs://ct-bancolombia/info-seguimiento/' + archivo, mifecha)
+#             if mensaje == "Corrio Full HD":
+#                 move(local_route + archivo, fileserver_baseroute + "/BI_Archivos/GOOGLE/Bancolombia_Admin/Seguimiento/Procesados/"+archivo)
+#                 response["code"] = 200
+#                 response["description"] = "Se realizo la peticion Full HD"
+#                 response["status"] = True
+
+#     return jsonify(response), response["code"]

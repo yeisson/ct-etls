b5caaf656e6a250be5fffb330f9025cb64a35dda
Andrés F Rave <arave@contentobps.com>
2019-04-08 16:15:38 -0500

    Acceso a GET por flask

--- procesos/Telefonia/login_logout.py
+++ procesos/Telefonia/login_logout.py
@@ -17,6 +17,7 @@
 ########################### LIBRERIAS #####################################
 from flask import Blueprint
 from flask import jsonify
+from flask import request
 from google.oauth2 import service_account
 from google.auth.transport.requests import AuthorizedSession
 from google.cloud import datastore

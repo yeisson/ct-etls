df7c87231a7e0587ace74b25cf80f699327d6d47
lcanor <andreacanorestrepo74@gmail.com>
2019-05-23 15:37:01 -0500

    cambio linead/epm  #coding: utf-8

--- procesos/epm.py
+++ procesos/epm.py
@@ -1,55 +1,55 @@
-from flask import Blueprint
-from flask import jsonify
-from shutil import copyfile, move
-from google.cloud import storage
-from google.cloud import bigquery
-import dataflow_pipeline.epm.epm_seguimiento_beam as epm_seguimiento_beam
-import os
-import socket
+# from flask import Blueprint
+# from flask import jsonify
+# from shutil import copyfile, move
+# from google.cloud import storage
+# from google.cloud import bigquery
+# import dataflow_pipeline.epm.epm_seguimiento_beam as epm_seguimiento_beam
+# import os
+# import socket
 
-avalcreditos_api = Blueprint('epm_api', __name__)
+# avalcreditos_api = Blueprint('epm_api', __name__)
 
-fileserver_baseroute = ("//192.168.20.87", "/media")[socket.gethostname()=="contentobi"]
+# fileserver_baseroute = ("//192.168.20.87", "/media")[socket.gethostname()=="contentobi"]
 
 
-@epm_api.route("/archivos_seguimiento")
-def archivos_Seguimiento():
+# @epm_api.route("/archivos_seguimiento")
+# def archivos_Seguimiento():
 
-    response = {}
-    response["code"] = 400
-    response["description"] = "No se encontraron ficheros"
-    response["status"] = False
+#     response = {}
+#     response["code"] = 400
+#     response["description"] = "No se encontraron ficheros"
+#     response["status"] = False
 
-    local_route = fileserver_baseroute + "/BI_Archivos/GOOGLE/Epm/Seguimiento/"
-    archivos = os.listdir(local_route)
-    for archivo in archivos:
-        if archivo.endswith(".csv"):
-            mifecha = archivo[17:25]
+#     local_route = fileserver_baseroute + "/BI_Archivos/GOOGLE/Epm/Seguimiento/"
+#     archivos = os.listdir(local_route)
+#     for archivo in archivos:
+#         if archivo.endswith(".csv"):
+#             mifecha = archivo[17:25]
 
-            storage_client = storage.Client()
-            bucket = storage_client.get_bucket('ct-epm')
+#             storage_client = storage.Client()
+#             bucket = storage_client.get_bucket('ct-epm')
 
-            # Subir fichero a Cloud Storage antes de enviarlo a procesar a Dataflow
-            blob = bucket.blob('info-seguimiento/' + archivo)
-            blob.upload_from_filename(local_route + archivo)
+#             # Subir fichero a Cloud Storage antes de enviarlo a procesar a Dataflow
+#             blob = bucket.blob('info-seguimiento/' + archivo)
+#             blob.upload_from_filename(local_route + archivo)
 
-            # Una vez subido el fichero a Cloud Storage procedemos a eliminar los registros de BigQuery
-            deleteQuery = "DELETE FROM `contento-bi.epm.seguimiento` WHERE fecha = '" + mifecha + "'"
+#             # Una vez subido el fichero a Cloud Storage procedemos a eliminar los registros de BigQuery
+#             deleteQuery = "DELETE FROM `contento-bi.epm.seguimiento` WHERE fecha = '" + mifecha + "'"
 
-            #Primero eliminamos todos los registros que contengan esa fecha
-            client = bigquery.Client()
-            query_job = client.query(deleteQuery)
+#             #Primero eliminamos todos los registros que contengan esa fecha
+#             client = bigquery.Client()
+#             query_job = client.query(deleteQuery)
 
-            #result = query_job.result()
-            query_job.result() # Corremos el job de eliminacion de datos de BigQuery
+#             #result = query_job.result()
+#             query_job.result() # Corremos el job de eliminacion de datos de BigQuery
 
-            # Terminada la eliminacion de BigQuery y la subida a Cloud Storage corremos el Job
-            mensaje = epm_seguimiento_beam.run('gs://ct-epm/info-seguimiento/' + archivo, mifecha)
-            if mensaje == "Corrio Full HD":
-                move(local_route + archivo, fileserver_baseroute + "/BI_Archivos/GOOGLE/Epm/Seguimiento/Procesados/"+archivo)
-                response["code"] = 200
-                response["description"] = "Se realizo la peticion Full HD"
-                response["status"] = True
+#             # Terminada la eliminacion de BigQuery y la subida a Cloud Storage corremos el Job
+#             mensaje = epm_seguimiento_beam.run('gs://ct-epm/info-seguimiento/' + archivo, mifecha)
+#             if mensaje == "Corrio Full HD":
+#                 move(local_route + archivo, fileserver_baseroute + "/BI_Archivos/GOOGLE/Epm/Seguimiento/Procesados/"+archivo)
+#                 response["code"] = 200
+#                 response["description"] = "Se realizo la peticion Full HD"
+#                 response["status"] = True
 
-    return jsonify(response), response["code"]
-    # return "Corriendo : " + mensaje
+#     return jsonify(response), response["code"]
+#     # return "Corriendo : " + mensaje

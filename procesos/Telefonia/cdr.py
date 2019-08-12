5bd089537ad7f413883090c789c1f786013f1c4a
Andrés F Rave <arave@contentobps.com>
2019-08-12 12:05:03 -0500

    Migración a nueva forma de time.

--- procesos/Telefonia/cdr.py
+++ procesos/Telefonia/cdr.py
@@ -24,27 +24,13 @@ cdr_api = Blueprint('cdr_api', __name__) #[[[[[[[[[[[[[[[[[[********************
 
 
 ########################### DEFINICION DE VARIABLES ###########################
-
-zona_horaria = (1, 2)[socket.gethostname()=="contentobi"]
-hoy = datetime.datetime.utcnow()
-ayer = datetime.datetime.today() - datetime.timedelta(days = zona_horaria)
-ano = str(hoy.year)
+fecha = time.strftime('%Y%m%d')
 hour1 = "000000"
 hour2 = "235959"
-if len(str(hoy.day)) == 1:
-    dia = "0" + str(hoy.day)
-else:
-    dia = str(hoy.day)
 
-if len(str(hoy.month)) == 1:
-    mes = "0"+ str(hoy.month)
-else:
-    mes = str(hoy.month)
+GetDate1 = time.strftime('%Y%m%d')+str(hour1)
+GetDate2 = time.strftime('%Y%m%d')+str(hour2)
 
-GetDate1 = str(ano)+str(mes)+str(dia)+str(hour1)
-GetDate2 = str(ano)+str(mes)+str(dia)+str(hour2)
-
-fecha = str(ano)+str(mes)+str(dia)
 KEY_REPORT = "cdr" #[[[[[[[[[[[[[[[[[[***********************************]]]]]]]]]]]]]]]]]]
 CODE_REPORT = "cbps_cdr" #[[[[[[[[[[[[[[[[[[***********************************]]]]]]]]]]]]]]]]]]
 Ruta = ("/192.168.20.87", "media")[socket.gethostname()=="contentobi"]
--- procesos/Telefonia/login_logout.py
+++ procesos/Telefonia/login_logout.py
@@ -17,33 +17,20 @@ import dataflow_pipeline.massive as pipeline
 import cloud_storage_controller.cloud_storage_controller as gcscontroller
 import datetime
 import time
+import sys
 import dataflow_pipeline.telefonia.login_logout_beam as login_logout_beam #[[[[[[[[[[[[[[[[[[***********************************]]]]]]]]]]]]]]]]]]
 
 login_logout_api = Blueprint('login_logout_api', __name__) #[[[[[[[[[[[[[[[[[[***********************************]]]]]]]]]]]]]]]]]]
 
 
 ########################### DEFINICION DE VARIABLES ###########################
-
-zona_horaria = (1, 2)[socket.gethostname()=="contentobi"]
-hoy = datetime.datetime.utcnow()
-ayer = datetime.datetime.today() - datetime.timedelta(days = zona_horaria)
-ano = str(hoy.year)
+fecha = time.strftime('%Y%m%d')
 hour1 = "000000"
 hour2 = "235959"
-if len(str(hoy.day)) == 1:
-    dia = "0" + str(hoy.day)
-else:
-    dia = str(hoy.day)
 
-if len(str(hoy.month)) == 1:
-    mes = "0"+ str(hoy.month)
-else:
-    mes = str(hoy.month)
+GetDate1 = time.strftime('%Y%m%d')+str(hour1)
+GetDate2 = time.strftime('%Y%m%d')+str(hour2)
 
-GetDate1 = str(ano)+str(mes)+str(dia)+str(hour1)
-GetDate2 = str(ano)+str(mes)+str(dia)+str(hour2)
-
-fecha = str(ano)+str(mes)+str(dia)
 KEY_REPORT = "login_logout" #[[[[[[[[[[[[[[[[[[***********************************]]]]]]]]]]]]]]]]]]
 CODE_REPORT = "login_time" #[[[[[[[[[[[[[[[[[[***********************************]]]]]]]]]]]]]]]]]]
 Ruta = ("/192.168.20.87", "media")[socket.gethostname()=="contentobi"]
@@ -56,76 +43,77 @@ ruta_completa = "/"+ Ruta +"/BI_Archivos/GOOGLE/Telefonia/"+ KEY_REPORT +"/" + f
 @login_logout_api.route("/" + KEY_REPORT, methods=['GET']) #[[[[[[[[[[[[[[[[[[***********************************]]]]]]]]]]]]]]]]]]
 def Ejecutar():
 
-    # storage_client = storage.Client()
-    # bucket = storage_client.get_bucket('ct-telefonia')
-    # gcs_path = 'gs://ct-telefonia'
-    # sub_path = KEY_REPORT + '/'
-    # output = gcs_path + "/" + sub_path + fecha + ext
-    # blob = bucket.blob(sub_path + fecha + ext)
-    # dateini = request.args.get('dateini')
-    # dateend = request.args.get('dateend')
+    reload(sys)
+    sys.setdefaultencoding('utf8')
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket('ct-telefonia')
+    gcs_path = 'gs://ct-telefonia'
+    sub_path = KEY_REPORT + '/'
+    output = gcs_path + "/" + sub_path + fecha + ext
+    blob = bucket.blob(sub_path + fecha + ext)
+    dateini = request.args.get('dateini')
+    dateend = request.args.get('dateend')
 
-    # if dateini is None:
-    #     dateini = GetDate1
-    # else:
-    #     dateini = dateini + hour1
+    if dateini is None:
+        dateini = GetDate1
+    else:
+        dateini = dateini + hour1
 
-    # if dateend is None:
-    #     dateend = GetDate2
-    # else:
-    #     dateend = dateend + hour2
+    if dateend is None:
+        dateend = GetDate2
+    else:
+        dateend = dateend + hour2
 
-    # client = bigquery.Client()
-    # QUERY = (
-    #     'SELECT servidor, operacion, token, ipdial_code, id_cliente, cartera FROM telefonia.parametros_ipdial') #WHERE ipdial_code = "intcob-unisabaneta"
-    # query_job = client.query(QUERY)
-    # rows = query_job.result()
-    # data = ""
+    client = bigquery.Client()
+    QUERY = (
+        'SELECT servidor, operacion, token, ipdial_code, id_cliente, cartera FROM telefonia.parametros_ipdial') #WHERE ipdial_code = "intcob-unisabaneta"
+    query_job = client.query(QUERY)
+    rows = query_job.result()
+    data = ""
     
-    # try:
-    #     os.remove(ruta_completa) #Eliminar de aries
-    # except: 
-    #     print("Eliminado de aries")
+    try:
+        os.remove(ruta_completa) #Eliminar de aries
+    except: 
+        print("Eliminado de aries")
     
-    # try:
-    #     blob.delete() #Eliminar del storage
-    # except: 
-    #     print("Eliminado de storage")
+    try:
+        blob.delete() #Eliminar del storage
+    except: 
+        print("Eliminado de storage")
 
-    # try:
-    #     QUERY2 = ('delete FROM `contento-bi.telefonia.login_logout` where date = ' + '"' + dateini[0:4] + '-' + dateini[4:-8] + '-' + dateini[6:-6] + '"')
-    #     query_job = client.query(QUERY2)
-    #     rows2 = query_job.result()
-    # except: 
-    #     print("Eliminado de bigquery")
+    try:
+        QUERY2 = ('delete FROM `contento-bi.telefonia.login_logout` where date = ' + '"' + dateini[0:4] + '-' + dateini[4:-8] + '-' + dateini[6:-6] + '"')
+        query_job = client.query(QUERY2)
+        rows2 = query_job.result()
+    except: 
+        print("Eliminado de bigquery")
 
-    # file = open(ruta_completa,"a")
-    # for row in rows:
-    #     url = 'http://' + str(row.servidor) + '/ipdialbox/api_reports.php?token=' + row.token + '&report=' + str(CODE_REPORT) + '&date_ini=' + dateini + '&date_end=' + dateend
-    #     datos = requests.get(url).content
-    #     if len(requests.get(url).content) < 40:
-    #         continue
-    #     else:
-    #         i = json.loads(datos)
-    #         for rown in i:
-    #             file.write(
-    #                 str(rown["date"])+"|"+
-    #                 str(rown["agent"])+"|"+
-    #                 str(rown["identification"])+"|"+
-    #                 str(rown["login_date"])+"|"+
-    #                 str(rown["logout_date"])+"|"+
-    #                 str(rown["login_time"])+"|"+
-    #                 row.ipdial_code.encode('utf-8') + "|" +
-    #                 str(row.id_cliente).encode('utf-8') + "|" +
-    #                 row.cartera.encode('utf-8') + "\n")
+    file = open(ruta_completa,"a")
+    for row in rows:
+        url = 'http://' + str(row.servidor) + '/ipdialbox/api_reports.php?token=' + row.token + '&report=' + str(CODE_REPORT) + '&date_ini=' + dateini + '&date_end=' + dateend
+        datos = requests.get(url).content
+        if len(requests.get(url).content) < 40:
+            continue
+        else:
+            i = json.loads(datos)
+            for rown in i:
+                file.write(
+                    str(rown["date"])+"|"+
+                    str(rown["agent"])+"|"+
+                    str(rown["identification"])+"|"+
+                    str(rown["login_date"])+"|"+
+                    str(rown["logout_date"])+"|"+
+                    str(rown["login_time"])+"|"+
+                    row.ipdial_code.encode('utf-8') + "|" +
+                    str(row.id_cliente).encode('utf-8') + "|" +
+                    row.cartera.encode('utf-8') + "\n")
     
-    # file.close()
-    # blob.upload_from_filename(ruta_completa)
-    # time.sleep(10)
-    # ejecutar = login_logout_beam.run(output, KEY_REPORT) #[[[[[[[[[[[[[[[[[[***********************************]]]]]]]]]]]]]]]]]]    
-    # time.sleep(60)
+    file.close()
+    blob.upload_from_filename(ruta_completa)
+    time.sleep(10)
+    ejecutar = login_logout_beam.run(output, KEY_REPORT) #[[[[[[[[[[[[[[[[[[***********************************]]]]]]]]]]]]]]]]]]    
+    time.sleep(60)
 
-    # return("El proceso " + KEY_REPORT + ". Fue Cargado Exitosamente en la fecha: " + fecha)
-    iterar = time.strftime('%d/%m/%y %H:%M:%S')
-    return (iterar)
+    return("El proceso " + KEY_REPORT + ". Fue Cargado Exitosamente en la fecha: " + fecha)
+    return (fecha)
 ########################################################################################################################

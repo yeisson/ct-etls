dfd6060b598341dc9a3b73a7ce961151da21d591
Andrés F Rave <arave@contentobps.com>
2019-08-12 10:57:15 -0500

    Prueba de iteración de fechas

--- procesos/Telefonia/login_logout.py
+++ procesos/Telefonia/login_logout.py
@@ -56,74 +56,76 @@ ruta_completa = "/"+ Ruta +"/BI_Archivos/GOOGLE/Telefonia/"+ KEY_REPORT +"/" + f
 @login_logout_api.route("/" + KEY_REPORT, methods=['GET']) #[[[[[[[[[[[[[[[[[[***********************************]]]]]]]]]]]]]]]]]]
 def Ejecutar():
 
-    storage_client = storage.Client()
-    bucket = storage_client.get_bucket('ct-telefonia')
-    gcs_path = 'gs://ct-telefonia'
-    sub_path = KEY_REPORT + '/'
-    output = gcs_path + "/" + sub_path + fecha + ext
-    blob = bucket.blob(sub_path + fecha + ext)
-    dateini = request.args.get('dateini')
-    dateend = request.args.get('dateend')
+    # storage_client = storage.Client()
+    # bucket = storage_client.get_bucket('ct-telefonia')
+    # gcs_path = 'gs://ct-telefonia'
+    # sub_path = KEY_REPORT + '/'
+    # output = gcs_path + "/" + sub_path + fecha + ext
+    # blob = bucket.blob(sub_path + fecha + ext)
+    # dateini = request.args.get('dateini')
+    # dateend = request.args.get('dateend')
 
-    if dateini is None:
-        dateini = GetDate1
-    else:
-        dateini = dateini + hour1
+    # if dateini is None:
+    #     dateini = GetDate1
+    # else:
+    #     dateini = dateini + hour1
 
-    if dateend is None:
-        dateend = GetDate2
-    else:
-        dateend = dateend + hour2
+    # if dateend is None:
+    #     dateend = GetDate2
+    # else:
+    #     dateend = dateend + hour2
 
-    client = bigquery.Client()
-    QUERY = (
-        'SELECT servidor, operacion, token, ipdial_code, id_cliente, cartera FROM telefonia.parametros_ipdial') #WHERE ipdial_code = "intcob-unisabaneta"
-    query_job = client.query(QUERY)
-    rows = query_job.result()
-    data = ""
+    # client = bigquery.Client()
+    # QUERY = (
+    #     'SELECT servidor, operacion, token, ipdial_code, id_cliente, cartera FROM telefonia.parametros_ipdial') #WHERE ipdial_code = "intcob-unisabaneta"
+    # query_job = client.query(QUERY)
+    # rows = query_job.result()
+    # data = ""
     
-    try:
-        os.remove(ruta_completa) #Eliminar de aries
-    except: 
-        print("Eliminado de aries")
+    # try:
+    #     os.remove(ruta_completa) #Eliminar de aries
+    # except: 
+    #     print("Eliminado de aries")
     
-    try:
-        blob.delete() #Eliminar del storage
-    except: 
-        print("Eliminado de storage")
+    # try:
+    #     blob.delete() #Eliminar del storage
+    # except: 
+    #     print("Eliminado de storage")
 
-    try:
-        QUERY2 = ('delete FROM `contento-bi.telefonia.login_logout` where date = ' + '"' + dateini[0:4] + '-' + dateini[4:-8] + '-' + dateini[6:-6] + '"')
-        query_job = client.query(QUERY2)
-        rows2 = query_job.result()
-    except: 
-        print("Eliminado de bigquery")
+    # try:
+    #     QUERY2 = ('delete FROM `contento-bi.telefonia.login_logout` where date = ' + '"' + dateini[0:4] + '-' + dateini[4:-8] + '-' + dateini[6:-6] + '"')
+    #     query_job = client.query(QUERY2)
+    #     rows2 = query_job.result()
+    # except: 
+    #     print("Eliminado de bigquery")
 
-    file = open(ruta_completa,"a")
-    for row in rows:
-        url = 'http://' + str(row.servidor) + '/ipdialbox/api_reports.php?token=' + row.token + '&report=' + str(CODE_REPORT) + '&date_ini=' + dateini + '&date_end=' + dateend
-        datos = requests.get(url).content
-        if len(requests.get(url).content) < 40:
-            continue
-        else:
-            i = json.loads(datos)
-            for rown in i:
-                file.write(
-                    str(rown["date"])+"|"+
-                    str(rown["agent"])+"|"+
-                    str(rown["identification"])+"|"+
-                    str(rown["login_date"])+"|"+
-                    str(rown["logout_date"])+"|"+
-                    str(rown["login_time"])+"|"+
-                    row.ipdial_code.encode('utf-8') + "|" +
-                    str(row.id_cliente).encode('utf-8') + "|" +
-                    row.cartera.encode('utf-8') + "\n")
+    # file = open(ruta_completa,"a")
+    # for row in rows:
+    #     url = 'http://' + str(row.servidor) + '/ipdialbox/api_reports.php?token=' + row.token + '&report=' + str(CODE_REPORT) + '&date_ini=' + dateini + '&date_end=' + dateend
+    #     datos = requests.get(url).content
+    #     if len(requests.get(url).content) < 40:
+    #         continue
+    #     else:
+    #         i = json.loads(datos)
+    #         for rown in i:
+    #             file.write(
+    #                 str(rown["date"])+"|"+
+    #                 str(rown["agent"])+"|"+
+    #                 str(rown["identification"])+"|"+
+    #                 str(rown["login_date"])+"|"+
+    #                 str(rown["logout_date"])+"|"+
+    #                 str(rown["login_time"])+"|"+
+    #                 row.ipdial_code.encode('utf-8') + "|" +
+    #                 str(row.id_cliente).encode('utf-8') + "|" +
+    #                 row.cartera.encode('utf-8') + "\n")
     
-    file.close()
-    blob.upload_from_filename(ruta_completa)
-    time.sleep(10)
-    ejecutar = login_logout_beam.run(output, KEY_REPORT) #[[[[[[[[[[[[[[[[[[***********************************]]]]]]]]]]]]]]]]]]    
-    time.sleep(60)
+    # file.close()
+    # blob.upload_from_filename(ruta_completa)
+    # time.sleep(10)
+    # ejecutar = login_logout_beam.run(output, KEY_REPORT) #[[[[[[[[[[[[[[[[[[***********************************]]]]]]]]]]]]]]]]]]    
+    # time.sleep(60)
 
-    return("El proceso " + KEY_REPORT + ". Fue Cargado Exitosamente en la fecha: " + fecha)
+    # return("El proceso " + KEY_REPORT + ". Fue Cargado Exitosamente en la fecha: " + fecha)
+    iterar = time.strftime('%d/%m/%y %H:%M:%S')
+    return (iterar)
 ########################################################################################################################

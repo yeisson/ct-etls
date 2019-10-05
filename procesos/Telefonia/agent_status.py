ca97e518960aacb9dc21568c581860cf601f39a7
Andrés F Rave <arave@contentobps.com>
2019-10-04 19:32:10 -0500

    no message

--- procesos/Telefonia/agent_status.py
+++ procesos/Telefonia/agent_status.py
@@ -66,7 +66,7 @@ def Ejecutar():
 
     client = bigquery.Client()
     QUERY = (
-        'SELECT servidor, operacion, token, ipdial_code, id_cliente, cartera FROM telefonia.parametros_ipdial') #WHERE ipdial_code = "intcob-unisabaneta"
+        'SELECT servidor, operacion, token, ipdial_code, id_cliente, cartera FROM telefonia.parametros_ipdial where Estado = "Activado"') #WHERE ipdial_code = "intcob-unisabaneta"
     query_job = client.query(QUERY)
     rows = query_job.result()
     data = ""
@@ -92,6 +92,8 @@ def Ejecutar():
     for row in rows:
         url = 'http://' + str(row.servidor) + '/ipdialbox/api_reports.php?token=' + row.token + '&report=' + str(CODE_REPORT) + '&date_ini=' + dateini + '&date_end=' + dateend
         datos = requests.get(url).content
+
+
         if len(requests.get(url).content) < 40:
             continue
         else:

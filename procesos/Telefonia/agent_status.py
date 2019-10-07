bab389a0a46439f9495fbf62fefa50f88e9cd8d4
Andrés F Rave <arave@contentobps.com>
2019-10-07 10:31:11 -0500

    Testeos!

--- procesos/Telefonia/agent_status.py
+++ procesos/Telefonia/agent_status.py
@@ -93,7 +93,6 @@ def Ejecutar():
         url = 'http://' + str(row.servidor) + '/ipdialbox/api_reports.php?token=' + row.token + '&report=' + str(CODE_REPORT) + '&date_ini=' + dateini + '&date_end=' + dateend
         datos = requests.get(url).content
 
-
         if len(requests.get(url).content) < 40:
             continue
         else:

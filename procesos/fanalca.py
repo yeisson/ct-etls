b28ac882771c8c3093870816f3399fbfee5440c0
Andrés F Rave <arave@contentobps.com>
2019-10-16 16:45:37 -0500

    cambio para fecha

--- procesos/fanalca.py
+++ procesos/fanalca.py
@@ -141,7 +141,7 @@ def gestion_ipdial():
     archivos = os.listdir(local_route)
     for archivo in archivos:
         if archivo.endswith(".csv"):
-            mifecha = archivo[17:23]
+            mifecha = archivo[17:25]
 
             storage_client = storage.Client()
             bucket = storage_client.get_bucket('ct-fanalca')

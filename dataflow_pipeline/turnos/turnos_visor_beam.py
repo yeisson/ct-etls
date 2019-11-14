42de6e5d091367230eb44b7961939cd927081707
Andrés F Rave <arave@contentobps.com>
2019-11-14 14:18:12 -0500

    cambios en tupla

--- dataflow_pipeline/turnos/turnos_visor_beam.py
+++ dataflow_pipeline/turnos/turnos_visor_beam.py
@@ -98,14 +98,7 @@ class formatearData(beam.DoFn):
 				'DESCANSOS' : arrayCSV[25],
 				'DIFERENCIA_DESCANSO' : arrayCSV[26],
 				'DIFERENCIA_DE_DESCANSO_INTERMEDIO' : arrayCSV[27],
-				'DIFERENCIA_DESCANSO_FINAL' : arrayCSV[28],
-
-
-
-
-
-
-
+				'DIFERENCIA_DESCANSO_FINAL' : arrayCSV[28]
 				}
 		
 		return [tupla]
@@ -124,7 +117,7 @@ def run(archivo, mifecha):
         "--temp_location", ("%s/dataflow_files/temp" % gcs_path),
         "--output", ("%s/dataflow_files/output" % gcs_path),
         "--setup_file", "./setup.py",
-        "--max_num_workers", "10",
+        "--max_num_workers", "5",
 		"--subnetwork", "https://www.googleapis.com/compute/v1/projects/contento-bi/regions/us-central1/subnetworks/contento-subnet1"
         # "--num_workers", "30",
         # "--autoscaling_algorithm", "NONE"		

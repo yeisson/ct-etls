a9cdc3ffe5dde7bd89dac64656c054be8b20ca6d
Andrés F Rave <arave@contentobps.com>
2019-11-26 17:35:09 -0500

    creación nuevos proyectos de rappi

--- procesos/rappi.py
+++ procesos/rappi.py
@@ -121,3 +121,87 @@ def Ejecutar():
 
     # return (url)
     return ("creo el archivo relajado")
+
+
+
+
+@rappi_api.route("/" + "formulario_rappi_sc", methods=['GET']) #[[[[[[[[[[[[[[[[[[***********************************]]]]]]]]]]]]]]]]]]
+def Ejecutar():
+
+    # ejemplo de consulta: http://35.239.77.81:5000/rappi/formulario_rappi?fechaInicial=2019-11-14&fechaFinal=2019-11-17
+
+    reload(sys)
+    sys.setdefaultencoding('utf8')
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket('ct-rappi')
+    gcs_path = 'gs://ct-rappi'
+    sub_path = 'formulario_rappi_'
+    fecha = time.strftime('%Y-%m-%d')
+    output = gcs_path + "/" + sub_path + fecha + ".csv"
+    blob = bucket.blob(sub_path + fecha + ".csv")
+    dateini = request.args.get('fechaInicial')
+    dateend = request.args.get('fechaFinal')
+    client = bigquery.Client()
+    Ruta = ("/192.168.20.87", "media")[socket.gethostname()=="contentobi"]
+    ruta_completa = "/"+ Ruta +"/BI_Archivos/GOOGLE/Rappi/"+ sub_path + fecha +".csv"
+
+    if dateini is None:
+        dateini = fecha
+        dateinip = str(dateini) + "%20" + "00:00:00"
+    else:   
+        dateini = dateini
+        dateinip = str(dateini) + "%20" + "00:00:00"
+
+    if dateend is None:
+        dateend = fecha
+        dateendp = str(dateend) + "%20" + "23:59:59"
+    else:
+        dateend = dateend
+        dateendp = str(dateend) + "%20" + "23:59:59"
+    
+    try:
+        os.remove(ruta_completa) #Eliminar de aries
+    except: 
+        print("Eliminado de aries")
+    
+    try:
+        blob.delete() #Eliminar del storage
+    except: 
+        print("Eliminado de storage")
+
+    try:
+        QUERY2 = ('delete from `Rappi.flujo_react` where substr(fecha,1,10) between ' + '"' + dateini + '"' + ' and "' + dateend + '"')
+        query_job = client.query(QUERY2)
+        rows2 = query_job.result()
+    except: 
+        print("Eliminado de bigquery")
+
+
+    url = "https://webapp.contentobps.com/app/rappi/Desarrollo/back/API.php?token=HUCQHCHMENUNETUP&tipo=ContactoNoEfectivo&fechaInicial="+dateinip+"&fechaFinal="+dateendp
+    datos = requests.get(url).content
+
+    if len(requests.get(url).content) < 50:
+        return jsonify("No hay datos entre estas fechas: " + (dateini) + " y " + (dateend))
+    else:
+        i = json.loads(datos)
+        file = open(ruta_completa,"a")
+        for row in i:
+            file.write(
+                ((str(row['id']).replace('\r\n','').replace('\n','')).replace('\r','')).encode('utf-8')+';'+
+                ((str(row['fecha']).replace('\r\n','').replace('\n','')).replace('\r','')).encode('utf-8')+';'+
+                ((str(row['accion']).replace('\r\n','').replace('\n','')).replace('\r','')).encode('utf-8')+';'+
+                ((str(row['idLupe']).replace('\r\n','').replace('\n','')).replace('\r','')).encode('utf-8')+';'+
+                ((str(row['idAgente']).replace('\r\n','').replace('\n','')).replace('\r','')).encode('utf-8')+';'+
+                ((str(row['correo']).replace('\r\n','').replace('\n','')).replace('\r','')).encode('utf-8')+';'+
+                ((str(row['telMarcado']).replace('\r\n','').replace('\n','')).replace('\r','')).encode('utf-8')+';'+
+                ((str(row['comentarioTipificacion']).replace('\r\n','').replace('\n','')).replace('\r','')).encode('utf-8')+';'+
+                ((str(row['codigoTipificacion']).replace('\r\n','').replace('\n','')).replace('\r','')).encode('utf-8')+"\n")
+    
+        file.close()
+        blob.upload_from_filename(ruta_completa)
+        time.sleep(10)
+        ejecutar = rappi_beam.run(output) 
+        time.sleep(60)
+
+    # return (url)
+    return ("creo el archivo relajado")

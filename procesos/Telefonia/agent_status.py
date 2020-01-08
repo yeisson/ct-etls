c8f1864fac280508ca8b9f89bc031d722305101d
Andrés Rave <arave@contentobps.com>
2020-01-07 20:07:47 -0500

    Cambio de prueba final sourcetree

--- procesos/Telefonia/agent_status.py
+++ procesos/Telefonia/agent_status.py
@@ -24,7 +24,7 @@ import dataflow_pipeline.telefonia.agent_status_beam as agent_status_beam #[[[[[
 agent_status_api = Blueprint('agent_status_api', __name__) #[[[[[[[[[[[[[[[[[[***********************************]]]]]]]]]]]]]]]]]]
 
 
-############################# DEFINICIOnN DE VARIABLES ###########################
+############################# DEFINICION DE VARIABLES ###########################
 fecha = time.strftime('%Y%m%d')
 hour1 = "000000"
 hour2 = "235959"

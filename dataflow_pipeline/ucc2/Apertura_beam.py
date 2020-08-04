9a2d3c6c14e82466d8c4937017e8b53bd4bbc896
abetancurg <abetancurg@contentobps.com>
2020-08-04 08:30:21 -0500

    informes generales ucc

---
+++ dataflow_pipeline/ucc2/Apertura_beam.py
@@ -0,0 +1,94 @@
+from __future__ import print_function, absolute_import
+import logging
+import re
+import json
+import requests
+import uuid
+import time
+import os
+import socket
+import argparse
+import uuid
+import datetime
+import apache_beam as beam
+from apache_beam.io import ReadFromText
+from apache_beam.io import WriteToText
+from apache_beam.io.filesystems import FileSystems
+from apache_beam.metrics import Metrics
+from apache_beam.metrics.metric import MetricsFilter
+from apache_beam import pvalue
+from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.options.pipeline_options import SetupOptions
+
+
+
+TABLE_SCHEMA = (
+                'EMAIL:STRING, '
+                'TOTAL_LECTURAS:STRING, '
+                'ID_CONTACTO:STRING, '
+                'PAIS:STRING, '
+                'CIUDAD:STRING, '
+                'LATITUD:STRING, '
+                'LONGITUD:STRING, '
+                'NOMBRE:STRING '
+
+
+	)
+
+
+class formatearData(beam.DoFn):
+    
+        def __init__(self, mifecha):
+		super(formatearData, self).__init__()
+		self.mifecha = mifecha
+	
+	def process(self, element):
+		arrayCSV = element.split(';')
+
+		tupla= {'idkey' : str(uuid.uuid4()),
+			'campana' : self.mifecha,
+                        'BASE' : str("Apertura"),
+                        'EMAIL' : arrayCSV[0],
+                        'TOTAL_LECTURAS' : arrayCSV[1],
+                        'ID_CONTACTO' : arrayCSV[2],
+                        'PAIS' : arrayCSV[3],
+                        'CIUDAD' : arrayCSV[4],
+                        'LATITUD' : arrayCSV[5],
+                        'LONGITUD' : arrayCSV[6],
+                        'NOMBRE' : arrayCSV[7]                        
+
+                }
+		
+		return [tupla]
+
+
+
+def run(archivo, mifecha):
+
+	gcs_path = "gs://ct-ucc" #Definicion de la raiz del bucket
+	gcs_project = "contento-bi"
+
+	mi_runer = ("DirectRunner", "DataflowRunner")[socket.gethostname()=="contentobi"]
+	pipeline =  beam.Pipeline(runner=mi_runer, argv=[
+        "--project", gcs_project,
+        "--staging_location", ("%s/dataflow_files/staging_location" % gcs_path),
+        "--temp_location", ("%s/dataflow_files/temp" % gcs_path),
+        "--output", ("%s/dataflow_files/output" % gcs_path),
+        "--setup_file", "./setup.py",
+        "--max_num_workers", "10",
+		"--subnetwork", "https://www.googleapis.com/compute/v1/projects/contento-bi/regions/us-central1/subnetworks/contento-subnet1"
+	])
+	
+	lines = pipeline | 'Lectura de Archivo' >> ReadFromText(archivo,skip_header_lines=1)
+	transformed = (lines | 'Formatear Data' >> beam.ParDo(formatearData(mifecha)))
+	transformed | 'Escritura a BigQuery ucc' >> beam.io.WriteToBigQuery(
+		gcs_project + ":ucc.Correo_Apertura", 
+		schema=TABLE_SCHEMA, 
+		create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED, 
+		write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND
+		)
+
+	jobObject = pipeline.run()
+	# jobID = jobObject.job_id()
+
+	return ("Corrio Full HD")

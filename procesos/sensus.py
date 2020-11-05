8d6291ef55fe8c6961c76b2a59ca3a8f451af25b
Andrés Rave <arave@contentobps.com>
2020-11-05 10:58:58 -0500

    Jordan sube el sensus.

--- procesos/sensus.py
+++ procesos/sensus.py
@@ -10,6 +10,9 @@ import dataflow_pipeline.sensus.sensus_dmobility_beam as sensus_dmobility_beam
 import dataflow_pipeline.sensus.sensus_dchat_beam as sensus_dchat_beam
 import dataflow_pipeline.sensus.sensus_dcorreo_beam as sensus_dcorreo_beam
 import dataflow_pipeline.sensus.sensus_banco_beam as sensus_banco_beam
+import dataflow_pipeline.sensus.sensus_metalal_beam as sensus_metalal_beam
+import dataflow_pipeline.sensus.sensus_poc_beam as sensus_poc_beam
+import dataflow_pipeline.sensus.sensus_tecnicos_beam as sensus_tecnicos_beam
 import os
 import socket
 
@@ -327,3 +330,133 @@ def banco():
                 response["status"] = True
 
     return jsonify(response), response["code"]
+
+#####################################################################################################
+
+@sensus_api.route("/metalal")
+def metalal():
+
+    response = {}
+    response["code"] = 400
+    response["description"] = "No se encontraron ficheros"
+    response["status"] = False
+
+    local_route = fileserver_baseroute + "/BI_Archivos/GOOGLE/Sensus/Aseguramiento/Metalal/"
+    archivos = os.listdir(local_route)
+    for archivo in archivos:
+        if archivo.endswith(".csv"):
+            mifecha = archivo[0:8]
+
+            storage_client = storage.Client()
+            bucket = storage_client.get_bucket('ct-sensus')
+
+            # Subir fichero a Cloud Storage antes de enviarlo a procesar a Dataflow
+            blob = bucket.blob('adh/' + archivo)
+            blob.upload_from_filename(local_route + archivo)
+
+            # Una vez subido el fichero a Cloud Storage procedemos a eliminar los registros de BigQuery
+            deleteQuery = "DELETE FROM `contento-bi.sensus.banco` WHERE fecha = '" + mifecha + "'"
+
+            #Primero eliminamos todos los registros que contengan esa fecha
+            client = bigquery.Client()
+            query_job = client.query(deleteQuery)
+
+            #result = query_job.result()
+            query_job.result() # Corremos el job de eliminacion de datos de BigQuery
+
+            # Terminada la eliminacion de BigQuery y la subida a Cloud Storage corremos el Job
+            mensaje = sensus_metalal_beam.run('gs://ct-sensus/adh/' + archivo, mifecha)
+            if mensaje == "Corrio Full HD":
+                move(local_route + archivo, fileserver_baseroute + "/BI_Archivos/GOOGLE/Sensus/Aseguramiento/Metalal/Procesados/"+archivo)
+                response["code"] = 200
+                response["description"] = "Se realizo la peticion Full HD"
+                response["status"] = True
+
+    return jsonify(response), response["code"]
+
+
+#####################################################################################################
+
+@sensus_api.route("/poc")
+def poc():
+
+    response = {}
+    response["code"] = 400
+    response["description"] = "No se encontraron ficheros"
+    response["status"] = False
+
+    local_route = fileserver_baseroute + "/BI_Archivos/GOOGLE/Sensus/Aseguramiento/Poc/"
+    archivos = os.listdir(local_route)
+    for archivo in archivos:
+        if archivo.endswith(".csv"):
+            mifecha = archivo[0:8]
+
+            storage_client = storage.Client()
+            bucket = storage_client.get_bucket('ct-sensus')
+
+            # Subir fichero a Cloud Storage antes de enviarlo a procesar a Dataflow
+            blob = bucket.blob('poc/' + archivo)
+            blob.upload_from_filename(local_route + archivo)
+
+            # Una vez subido el fichero a Cloud Storage procedemos a eliminar los registros de BigQuery
+            deleteQuery = "DELETE FROM `contento-bi.sensus.poc` WHERE fecha = '" + mifecha + "'"
+
+            #Primero eliminamos todos los registros que contengan esa fecha
+            client = bigquery.Client()
+            query_job = client.query(deleteQuery)
+
+            #result = query_job.result()
+            query_job.result() # Corremos el job de eliminacion de datos de BigQuery
+
+            # Terminada la eliminacion de BigQuery y la subida a Cloud Storage corremos el Job
+            mensaje = sensus_poc_beam.run('gs://ct-sensus/poc/' + archivo, mifecha)
+            if mensaje == "Corrio Full HD":
+                move(local_route + archivo, fileserver_baseroute + "/BI_Archivos/GOOGLE/Sensus/Aseguramiento/Poc/Procesados/"+archivo)
+                response["code"] = 200
+                response["description"] = "Se cargaron los ficheros exitosamente"
+                response["status"] = True
+
+    return jsonify(response), response["code"]    
+
+#####################################################################################################
+
+@sensus_api.route("/tecnicos")
+def tecnicos():
+
+    response = {}
+    response["code"] = 400
+    response["description"] = "No se encontraron ficheros"
+    response["status"] = False
+
+    local_route = fileserver_baseroute + "/BI_Archivos/GOOGLE/Sensus/Aseguramiento/tecnicos/"
+    archivos = os.listdir(local_route)
+    for archivo in archivos:
+        if archivo.endswith(".csv"):
+            mifecha = archivo[0:8]
+
+            storage_client = storage.Client()
+            bucket = storage_client.get_bucket('ct-sensus')
+
+            # Subir fichero a Cloud Storage antes de enviarlo a procesar a Dataflow
+            blob = bucket.blob('tecnicos/' + archivo)
+            blob.upload_from_filename(local_route + archivo)
+
+            # Una vez subido el fichero a Cloud Storage procedemos a eliminar los registros de BigQuery
+            deleteQuery = "DELETE FROM `contento-bi.sensus.tecnicos` WHERE fecha = '" + mifecha + "'"
+
+            #Primero eliminamos todos los registros que contengan esa fecha
+            client = bigquery.Client()
+            query_job = client.query(deleteQuery)
+
+            #result = query_job.result()
+            query_job.result() # Corremos el job de eliminacion de datos de BigQuery
+
+            # Terminada la eliminacion de BigQuery y la subida a Cloud Storage corremos el Job
+            mensaje = sensus_tecnicos_beam.run('gs://ct-sensus/tecnicos/' + archivo, mifecha)
+            if mensaje == "Corrio Full HD":
+                move(local_route + archivo, fileserver_baseroute + "/BI_Archivos/GOOGLE/Sensus/Aseguramiento/tecnicos/Procesados/"+archivo)
+                response["code"] = 200
+                response["description"] = "Se cargaron los ficheros exitosamente"
+                response["status"] = True
+
+    return jsonify(response), response["code"]    
